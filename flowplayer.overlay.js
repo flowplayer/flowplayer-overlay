9a545b8537ebc0706a7da533961e9e585f6c08d1
Niklas Närhinen <niklas@narhinen.net>
2016-08-12 07:31:31 +0300

    Initial version

---
+++ flowplayer.overlay.js
@@ -0,0 +1,37 @@
+ /* global flowplayer */
+
+var common = flowplayer.common
+  , bean = flowplayer.bean;
+
+flowplayer(function(api, root) {
+  if (!api.conf.overlay) return;
+  api.conf.splash = true;
+  common.addClass(root, 'is-overlaid');
+  common.addClass(root, 'is-closeable');
+
+  var trigger = api.conf.overlay.trigger || api.conf.overlay;
+
+  var wrapper = document.createElement('div');
+  wrapper.className = 'flowplayer-overlay-mask';
+  wrapper.appendChild(root);
+
+  bean.on(document, 'click', trigger, function() {
+    showOverlay();
+    api.load();
+  });
+
+  api.on('unload', function() {
+    hideOverlay();
+  });
+
+
+  function showOverlay() {
+    document.body.appendChild(wrapper);
+    common.addClass(root, 'is-open');
+  }
+
+  function hideOverlay() {
+    if (wrapper.parentNode) document.body.removeChild(wrapper);
+    common.removeClass(root, 'is-open');
+  }
+});
