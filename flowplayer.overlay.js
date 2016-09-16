17467b05e0af2eb4f761c7bc91e752e27ecfb0b4
Niklas Närhinen <niklas@narhinen.net>
2016-09-16 03:01:41 +0300

    CommonJS export

--- flowplayer.overlay.js
+++ flowplayer.overlay.js
@@ -9,51 +9,54 @@
 */
 
 (function() {
-  /* global flowplayer */
-  var common = flowplayer.common
-    , bean = flowplayer.bean;
+  var extension = function(flowplayer) {
+    var common = flowplayer.common
+      , bean = flowplayer.bean;
 
-  flowplayer(function(api, root) {
-    if (!api.conf.overlay) return;
-    api.conf.splash = api.conf.autoplay = true;
-    common.addClass(root, 'is-overlaid');
-    common.addClass(root, 'is-closeable');
+    flowplayer(function(api, root) {
+      if (!api.conf.overlay) return;
+      api.conf.splash = api.conf.autoplay = true;
+      common.addClass(root, 'is-overlaid');
+      common.addClass(root, 'is-closeable');
 
-    var vendor = api.conf.overlay.vendor || 'native';
-    if (!flowplayer.overlay[vendor]) throw new Error('Overlay "' + vendor + '" not implemented');
-    flowplayer.overlay[vendor](api, root);
-  });
-
-
-  flowplayer.overlay = {};
-  flowplayer.overlay.native = function(api, root) {
-    var trigger = api.conf.overlay.trigger || api.conf.overlay
-      , wrapper = document.createElement('div');
-    wrapper.className = 'flowplayer-overlay-mask';
-    wrapper.appendChild(root);
-
-    bean.on(document, 'click', trigger, function() {
-      showOverlay();
-      api.load();
-    });
-
-    bean.on(document, 'keydown', function(ev) {
-      if (ev.keyCode === 27) api.unload();
-    });
-
-    api.on('unload', function() {
-      hideOverlay();
+      var vendor = api.conf.overlay.vendor || 'native';
+      if (!flowplayer.overlay[vendor]) throw new Error('Overlay "' + vendor + '" not implemented');
+      flowplayer.overlay[vendor](api, root);
     });
 
 
-    function showOverlay() {
-      document.body.appendChild(wrapper);
-      common.addClass(root, 'is-open');
-    }
+    flowplayer.overlay = {};
+    flowplayer.overlay.native = function(api, root) {
+      var trigger = api.conf.overlay.trigger || api.conf.overlay
+        , wrapper = document.createElement('div');
+      wrapper.className = 'flowplayer-overlay-mask';
+      wrapper.appendChild(root);
 
-    function hideOverlay() {
-      if (wrapper.parentNode) document.body.removeChild(wrapper);
-      common.removeClass(root, 'is-open');
-    }
+      bean.on(document, 'click', trigger, function() {
+        showOverlay();
+        api.load();
+      });
+
+      bean.on(document, 'keydown', function(ev) {
+        if (ev.keyCode === 27) api.unload();
+      });
+
+      api.on('unload', function() {
+        hideOverlay();
+      });
+
+
+      function showOverlay() {
+        document.body.appendChild(wrapper);
+        common.addClass(root, 'is-open');
+      }
+
+      function hideOverlay() {
+        if (wrapper.parentNode) document.body.removeChild(wrapper);
+        common.removeClass(root, 'is-open');
+      }
+    };
   };
+  if (typeof module === 'object' && module.exports) module.exports = extension;
+  else if (typeof flowplayer === 'function') extension(window.flowplayer);
 })();
